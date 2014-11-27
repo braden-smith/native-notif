49cf362c175c6558d7d15008b8412a401a09ccb3
braden-smith <github@bradensmith.com>
2014-11-27 13:47:50 +1100

    Update Natnot.java

--- src/android/Natnot.java
+++ src/android/Natnot.java
@@ -7,8 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.os.Vibrator;
-import android.content.Context;
+//import android.os.Vibrator;
+//import android.content.Context;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -17,20 +17,17 @@ public class Natnot extends CordovaPlugin {
 
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		
-		if (action.equals("natnot")) {
+		if (action.equals("createBasic")) {
 			String message = args.getString(0);
-			this.natnot(message, callbackContext);
+			this.createBasic(message, callbackContext);
 			return true;
 		}
 		return false;
 	}
 
-	private void natnot(String message, CallbackContext callbackContext) {
-		
+	private void createBasic(String message, CallbackContext callbackContext) {
 		//Vibrator vibrator = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
 		//vibrator.vibrate(1000);
-		
 		if (message != null && message.length() > 0) {
 			callbackContext.success(message);
 		} else {
