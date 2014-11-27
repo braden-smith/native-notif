62bc1601e32e87b3838813026424ef4fb3f0d18c
braden-smith <github@bradensmith.com>
2014-11-27 14:34:39 +1100

    Update Natnot.java

--- src/android/Natnot.java
+++ src/android/Natnot.java
@@ -7,15 +7,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.os.Vibrator;
-import android.content.Context;
+//import android.os.Vibrator;
+//import android.content.Context;
 
-/* import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.NotificationCompat.WearableExtender;
-import android.view.View;
+//import android.view.View;
 import android.app.PendingIntent;
-import android.content.Intent; */
+import android.content.Intent;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -33,8 +33,9 @@ public class Natnot extends CordovaPlugin {
 	}
 
 	private void createBasic(String message, CallbackContext callbackContext) {
-		Vibrator vibrator = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-		vibrator.vibrate(1000);
+		//Vibrator vibrator = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+		//vibrator.vibrate(1000);
+		genAlert(message);
 		if (message != null && message.length() > 0) {
 			callbackContext.success(message);
 		} else {
@@ -42,27 +43,25 @@ public class Natnot extends CordovaPlugin {
 		}
 	}
 	
-/*      public void onc_btnalert()
-    {
-
-        //build notification
-        int notificationId = 001;
-        Intent viewIntent = new Intent(this, CordovaApp.class);
-        PendingIntent viewPendingIntent =
-                PendingIntent.getActivity(this, 0, viewIntent, 0);
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(android.R.drawable.stat_notify_sync_noanim)
-                        .setContentTitle("ContentTitle")
-                        .setContentText("ContentText")
-                        .setContentIntent(viewPendingIntent);
-
-        // Get an instance of the NotificationManager service
-        NotificationManagerCompat notificationManager =
-                NotificationManagerCompat.from(this);
-        // Build the notification and issues it with notification manager.
-        notificationManager.notify(notificationId, notificationBuilder.build());
-    } */
+	private void genAlert(String message) {
+	        //build notification
+	        int notificationId = 001;
+	        Intent viewIntent = new Intent(this, Natnot.class);
+	        PendingIntent viewPendingIntent =
+	                PendingIntent.getActivity(this, 0, viewIntent, 0);
+	
+	        NotificationCompat.Builder notificationBuilder =
+	                new NotificationCompat.Builder(this)
+	                        .setSmallIcon(android.R.drawable.stat_notify_sync_noanim)
+	                        .setContentTitle("ContentTitle")
+	                        .setContentText("ContentText")
+	                        .setContentIntent(viewPendingIntent);
+	
+	        // Get an instance of the NotificationManager service
+	        NotificationManagerCompat notificationManager =
+	                NotificationManagerCompat.from(this);
+	        // Build the notification and issues it with notification manager.
+	        notificationManager.notify(notificationId, notificationBuilder.build());
+	}
 	
 }
