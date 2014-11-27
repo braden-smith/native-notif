cd166ebb662ef99e324ca14f9131c8e7bd846d9e
braden-smith <github@bradensmith.com>
2014-11-27 21:04:57 +1100

    Update Natnot.js

--- www/Natnot.js
+++ www/Natnot.js
@@ -1,18 +1,17 @@
 var natnot =  {
-    sendBasic: function(str, successCallback, errorCallback) {
+    sendBasic: function(strContentTitle, strContentText, strContentTitleBig, strContentQR, successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'Natnot', // mapped to our native Java class called "Calendar"
             'createBasic', // with this action name
-            [str]
+            [{
+                "contentTitle": strContentTitle,
+                "contentText": strContentText,
+                "contentTitleBig": strContentTitleBig,
+                "contentQR": strContentQR
+            }]
         );
     }
 }
 module.exports = natnot;
-
-//window.natnot = function(str, callback) {
-//        cordova.exec(callback, function(err) {
-//            callback('Nothing to echo.');
-//        }, "Natnot", "natnot", [str]);
-//    };
