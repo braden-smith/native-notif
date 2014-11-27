383ab8b95d015db861f02837cb1a513ce83d5e03
braden-smith <github@bradensmith.com>
2014-11-27 17:04:45 +1100

    Update Natnot.java

--- src/android/Natnot.java
+++ src/android/Natnot.java
@@ -7,16 +7,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-//import android.os.Vibrator;
-//import android.content.Context;
-
+//Notification specific
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.NotificationCompat.WearableExtender;
-//import android.view.View;
+
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.Context;
+//QR specific
+import android.graphics.Bitmap;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+//Vibration specific
+//import android.os.Vibrator;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -45,22 +49,42 @@ public class Natnot extends CordovaPlugin {
 	}
 	
 	private void genAlert(String message) {
+		//prepare QR
+		String qrData = "abcdefgh";
+        	int qrCodeDimension = 250;
+		QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(qrData, null,
+                	Contents.Type.TEXT, BarcodeFormat.QR_CODE.toString(), qrCodeDimension);
+                Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+                
 	        //build notification
 	        int notificationId = 001;
-	        //Intent viewIntent = new Intent(this, Natnot.class);
-	        //Context context=this.cordova.getActivity().getApplicationContext();
     		Context context=cordova.getActivity().getApplicationContext();
     		Intent viewIntent=new Intent(context,Natnot.class);
 	        
 	        PendingIntent viewPendingIntent =
 	                PendingIntent.getActivity(this.cordova.getActivity(), 0, viewIntent, 0);
-	
+	                
+		NotificationCompat.Builder notificationBuilder =
+		    new NotificationCompat.Builder(this.cordova.getActivity())
+		            .setSmallIcon(android.R.drawable.stat_notify_sync_noanim)
+		            .setLargeIcon(bitmap)
+		            .setContentTitle("TRS")
+		            .setContentText("Claim after immigration")
+		            .setContentIntent(viewPendingIntent)
+		            .setStyle(new NotificationCompat.BigPictureStyle()
+		                    .bigPicture(bitmap)
+		                    .bigLargeIcon(bitmap)
+		                    .setBigContentTitle("Tourist Refund Scheme"))
+		;
+
+		/*
 	        NotificationCompat.Builder notificationBuilder =
 	                new NotificationCompat.Builder(this.cordova.getActivity())
 	                        .setSmallIcon(android.R.drawable.stat_notify_sync_noanim)
 	                        .setContentTitle("ContentTitle")
 	                        .setContentText("ContentText")
 	                        .setContentIntent(viewPendingIntent);
+	        */
 	
 	        // Get an instance of the NotificationManager service
 	        NotificationManagerCompat notificationManager =
